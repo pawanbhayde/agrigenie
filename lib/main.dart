e0a08464f5740905280e83e67b24ae52418e7402
Pawan Bhayde <82137686+pawanbhayde@users.noreply.github.com>
2023-06-28 13:36:55 +0530

    2806

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:agrigenie/pages/navigator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+  
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
